4201fcd8f24a973c9ae6fa924d8ec6450d647360
purin_gidai <24.m.honma.nutfes@gmail.com>
2025-04-25 01:05:29 +0900

    first commit

---
+++ app/hello.go
@@ -0,0 +1,13 @@
+package main
+
+import(
+	 "fmt" // これを定義する大事！！
+)
+func main() {
+	takashi()
+	fmt.Println("Hello, World!")
+}
+
+func takashi() {
+	fmt.Println("Hello")
+}
---
+++ app/package.go
@@ -0,0 +1,12 @@
+// サンプルコード pakage.go
+
+package bava //これを定義する大事！！
+
+import (
+    "fmt"
+    "math/rand"
+)
+
+func Takashi() {
+    fmt.Println("My favorite number is", rand.Intn(10))
+}
